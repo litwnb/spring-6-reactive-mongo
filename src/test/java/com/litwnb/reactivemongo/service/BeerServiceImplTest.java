270c9b03936a9888a7719eb462544119d1fbc79c
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-06-02 12:56:23 +0300

    refactor test with awaitility

--- src/test/java/com/litwnb/reactivemongo/service/BeerServiceImplTest.java
+++ src/test/java/com/litwnb/reactivemongo/service/BeerServiceImplTest.java
@@ -10,8 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 class BeerServiceImplTest {
@@ -30,12 +31,17 @@ class BeerServiceImplTest {
 
 
     @Test
-    void saveBeer() throws InterruptedException {
+    void saveBeer() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
         Mono<BeerDTO> savedMono = beerService.saveBeer(Mono.just(beerDTO));
 
-        savedMono.subscribe(savedDto -> System.out.println(savedDto.getId()));
+        savedMono.subscribe(savedDto -> {
+            System.out.println(savedDto.getId());
+            atomicBoolean.set(true);
+        });
 
-        Thread.sleep(1000L);
+        await().untilTrue(atomicBoolean);
     }
 
     public static Beer getTestBeer() {
