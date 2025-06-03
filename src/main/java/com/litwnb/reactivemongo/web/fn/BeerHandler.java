348af9d3a7640656581eb213e18076cc2339ed7a
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-06-03 12:15:56 +0300

    added handler class

---
+++ src/main/java/com/litwnb/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,20 @@
+package com.litwnb.reactivemongo.web.fn;
+
+import com.litwnb.reactivemongo.model.BeerDTO;
+import com.litwnb.reactivemongo.service.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
